1a219bc235ab0c25f0c81e475873fda04a8ad0f7
xavier_ng <xavierngaba@gmail.com>
2017-10-30 23:11:28 +0100

    ajout d'un fichier de test avec robot framework

--- src/main/java/org/gestion/banque/test/BanqueKeywordLibrary.java
+++ src/main/java/org/gestion/banque/test/BanqueKeywordLibrary.java
@@ -1,13 +1,21 @@
 package org.gestion.banque.test;
 
 
+import java.util.Date;
+import org.gestion.banque.entity.Client;
+import org.gestion.banque.entity.Compte;
+import org.gestion.banque.entity.CompteCourant;
+import org.gestion.banque.entity.Employe;
 import org.gestion.banque.metier.IClientMetier;
 import org.gestion.banque.metier.ICompteMetier;
 import org.gestion.banque.metier.IEmployeMetier;
 import org.gestion.banque.metier.IGroupeMetier;
 import org.gestion.banque.metier.IOperationMetier;
+import org.robotframework.javalib.annotation.RobotKeyword;
+import org.robotframework.javalib.annotation.RobotKeywords;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+@RobotKeywords
 public class BanqueKeywordLibrary {
 	
 	public static final String ROBOT_LIBRARY_SCOPE = "GLOBAL";
@@ -19,5 +27,40 @@ public class BanqueKeywordLibrary {
 	IGroupeMetier gestionnaireGroupe = (IGroupeMetier) context.getBean("GroupeMetier");
 	IOperationMetier gestionnaireOperation = (IOperationMetier) context.getBean("OperationMetier");
 	
+	private Client cl;
+	private Employe emp;
+	private Compte cpte;
 	
+	/**
+	 * Mots-clé pour ajouter un client
+	 * @param nomClient
+	 * @param adresseClient
+	 */
+	@RobotKeyword
+	public void creerUnNouveauClient(String nomClient,String adresseClient){
+		cl = gestionnaireClient.addClient(new Client(nomClient,adresseClient));
+	}
+	
+	/**
+	 * Mots-clé pour ajouter un employé
+	 * @param nomEmploye
+	 * @param codeEmployeSup
+	 */
+	@RobotKeyword
+	private void creerUnNouvelEmploye(String nomEmploye,Long codeEmployeSup){
+		emp = gestionnaireEmploye.addEmploye(new Employe(nomEmploye), codeEmployeSup);
+	}
+	
+	
+	/**
+	 * Mots-clé pour creer un compte
+	 * @param solde
+	 * @param decouvert
+	 * @param codeClient
+	 * @param codeEmploye
+	 */
+	@RobotKeyword
+	private void creerUnNouveauCompte(double solde,double decouvert,Long codeClient,Long codeEmploye){
+		cpte = gestionnaireCompte.addCompte(new CompteCourant("Cpte1",new Date(),solde,decouvert), codeClient, codeEmploye);
+	}
 }
